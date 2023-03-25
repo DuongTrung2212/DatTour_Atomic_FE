d7f361d83f86de4b6c3b9e2e0f67d55b9d1027de
DuongTrung2212 <duongtrungqb12@gmail.com>
2023-03-25 22:20:22 +0700

    OK

--- src/content/Header/index.js
+++ src/content/Header/index.js
@@ -5,10 +5,7 @@ import {
     faSignOut,
     faTicket,
     faUser,
-<<<<<<< HEAD
     faUserGear,
-=======
->>>>>>> e168df4e6480efc6b21e7ff30a6a6bce9655b8ac
     faWalking,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -26,12 +23,9 @@ import HeadlessTippy from "@tippyjs/react/headless";
 import Tippy from "@tippyjs/react";
 import "react-toastify/dist/ReactToastify.css";
 import { TourContext } from "../../layouts/MainLayout/MainLayout";
-<<<<<<< HEAD
+
 import { AdminContext, UserContext } from "../../App";
-=======
-import { UserContext } from "../../App";
-import { async } from "@firebase/util";
->>>>>>> e168df4e6480efc6b21e7ff30a6a6bce9655b8ac
+
 import MenuItem from "./MenuItem/MenuItem";
 const cx = classNames.bind(styles);
 
@@ -238,7 +232,7 @@ function Header(props) {
                                             />
                                         );
                                     })}
-<<<<<<< HEAD
+
                                     {isAdmin ? (
                                         <MenuItem
                                             to={`/admin`}
@@ -252,8 +246,7 @@ function Header(props) {
                                     ) : (
                                         ""
                                     )}
-=======
->>>>>>> e168df4e6480efc6b21e7ff30a6a6bce9655b8ac
+
                                     <div
                                         className={cx("logOut")}
                                         onClick={handleLogOut}
