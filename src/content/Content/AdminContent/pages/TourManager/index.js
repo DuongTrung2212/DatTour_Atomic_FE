09058668b3d7d7ebfabdefe577a62c9ca1362b74
DuongTrung2212 <duongtrungqb12@gmail.com>
2023-04-07 16:26:58 +0700

    Fix err fetch  tour

--- src/content/Content/AdminContent/pages/TourManager/index.js
+++ src/content/Content/AdminContent/pages/TourManager/index.js
@@ -18,7 +18,7 @@ function TourManager() {
         await requestAxios
             .get(`tour`)
             .then((res) => {
-                setTourList(res.data.listTour);
+                if (res.data.listTour) setTourList(res.data.listTour);
             })
             .catch((err) => {
                 console.log("Errr get all tour");
